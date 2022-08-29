cc7c1de7faadeea48b35eacab0a31b3d05f1afc0
minuda <min9288@gmail.com>
2022-08-30 00:21:13 +0900

    UPDATE : 프로젝트 초기설정 및 로컬 DB연결

--- src/main/java/com/DietfriendsTodoDemoApp/DietfriendsTodoDemoAppApplication.java
+++ src/main/java/com/DietfriendsTodoDemoApp/DietfriendsTodoDemoAppApplication.java
@@ -2,12 +2,33 @@ package com.DietfriendsTodoDemoApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DietfriendsTodoDemoAppApplication {
 
+//	public static void main(String[] args) {
+//		SpringApplication.run(DietfriendsTodoDemoAppApplication.class, args);
+//	}
+
+	public static final String APPLICATION_LOCATIONS = "spring.config.additional-location="
+			+"classpath:application.yml,"
+			+"app/config/Dietfriends-Todo-Demo-App/springInfo-application.yml";
+//			+"/app/config/Dietfriends-Todo-Demo-App/springInfo-application.yml";  서버용
+
 	public static void main(String[] args) {
-		SpringApplication.run(DietfriendsTodoDemoAppApplication.class, args);
+		new SpringApplicationBuilder(DietfriendsTodoDemoAppApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
+	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 
 }
---
+++ src/main/java/com/DietfriendsTodoDemoApp/security/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.DietfriendsTodoDemoApp.security;
+
+public class SecurityConfig {
+}
