ed10bcb46f5a9a0ff9647842cda1b73353fb2b2e
minuda <min9288@gmail.com>
2022-08-29 22:06:50 +0900

    Initial commit

---
+++ src/main/java/com/DietfriendsTodoDemoApp/DietfriendsTodoDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.DietfriendsTodoDemoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietfriendsTodoDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietfriendsTodoDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DietfriendsTodoDemoApp/DietfriendsTodoDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DietfriendsTodoDemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietfriendsTodoDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
