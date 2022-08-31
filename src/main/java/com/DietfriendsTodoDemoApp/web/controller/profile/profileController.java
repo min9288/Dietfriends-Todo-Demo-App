39ae08bb968afb39b57ed7199c5a97d104b5fe1b
minuda <min9288@gmail.com>
2022-08-31 15:48:25 +0900

    UPDATE : CI/CD 배포 과정을 위한 초기 세팅

---
+++ src/main/java/com/DietfriendsTodoDemoApp/web/controller/profile/profileController.java
@@ -0,0 +1,24 @@
+package com.DietfriendsTodoDemoApp.web.controller.profile;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class profileController {
+
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
+    }
+
+}
